3b5f896295869087beb2dc91938345857ddbd13c
Pho Hale <CommanderPho@users.noreply.github.com>
2023-02-07 17:19:00 -0500

    convert to poetry

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,5 @@
-python -m pip install --user --upgrade setuptools wheel
-
+python -m pip install --upgrade setuptools wheel
 python setup.py sdist bdist_wheel
 
-python -m pip install --user --upgrade twine
-
-python -m twine upload --repository pypi dist/*
+@REM python -m pip install --upgrade twine
+@REM python -m twine upload --repository pypi dist/*
